87a436766a13e66c15ba4d4438ae8e84635ea9b6
ryu-hub <55954404+ryu-hub@users.noreply.github.com>
2019-10-06 18:36:59 +0900

    Answer

--- main.c
+++ main.c
@@ -3,6 +3,11 @@
 int main(int argc, char *argv[]) 
 {
     char str[5];
+    str[0]=6;
+    str[1]=9;
+    str[2]=-2;
+    str[3]=5;
+    str[4]=-3;
     
     while(fgets(str, sizeof(str), stdin)){
         printf("%s", str);
